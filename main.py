6141bd493b58504ec5c8f169cbb6f760bb1055fc
gprando <gprando55@gmail.com>
2022-03-23 13:37:24 -0300

    feat: scraping de todos os eventos de uma vez so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 from bs4 import BeautifulSoup
 import requests
 
@@ -6,36 +7,76 @@ url = 'http://apl.utfpr.edu.br/extensao/certificados/listaPublica'
 year=2013
 now = datetime.datetime.now()
 
-# event_names = []
+event_ids = []
 
-# while(year<=now.year):
-# 	_params = {'txtAno' : year}
-# 	html_doc = requests.post(url, data=_params)
+items = []
 
-# 	soup = BeautifulSoup(html_doc.text, 'html.parser')
+while(year<=2014):
+	_params = {'txtAno' : year}
+	html_doc = requests.post(url, data=_params)
 
-# 	select = soup.find('select', {'class' : 'combo', 'name': 'txtEvento'})
-# 	for option in select.stripped_strings:
-# 		event_names.append(option)
-# 	year+=1
+	soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-# # event_names.remove('Todos')
+	select = soup.find('select', {'class' : 'combo', 'name': 'txtEvento'})
+	for option in select.find_all('option'):
+		event_ids.append(option['value'])
+	year+=1
 
-# print(event_names)
-_params = {'txtEvento' : '6679'}
-html_doc = requests.post(url, data=_params)
-soup = BeautifulSoup(html_doc.text, 'html.parser')
-tags_tr = soup.find_all('tr')
+print('number of events ', len(event_ids))
 
-# remover cabeçalho
-tags_tr.pop(0)
+event_ids.remove('')
+# event_ids = ['2494']
 
-for person in tags_tr:
-	tags_tds = person.find_all('td')
-	name = tags_tds[0].text.strip() 
-	certificate = tags_tds[1].text.strip() 
-	certificate_link = person.find_all('a', href=True) 
-	print(name)
-	print(certificate)
-	print(certificate_link[0]['href'])
-	print('\n')
+for event_id in event_ids: 
+	if not event_id.isnumeric():
+		continue
+
+	_params = {'txtEvento' : ''+event_id+''}
+
+	print('find all certificates for event id', event_id)
+
+	while True:
+		html_doc = requests.post(url, data=_params)
+		soup = BeautifulSoup(html_doc.text, 'html.parser')
+
+		found_certificates = soup.find('b', string='Não foram encontrados certificados válidos para emissão')
+		
+		has_certificates = found_certificates == None
+		print('has_certificates', has_certificates)
+		
+		if not has_certificates:
+			break
+
+		tags_tr = soup.find_all('tr')
+
+		# remover cabeçalho
+		tags_tr.pop(0)
+
+		for person in tags_tr:
+
+			tags_tds = person.find_all('td')
+			name = tags_tds[0].text.strip() 
+			certificate = tags_tds[1].text.strip() 
+			certificate_link = person.find_all('a', href=True)
+
+			item = { 'name': name, 'certificate': certificate, 'certificate_link': certificate_link[0]['href'] }
+
+			items.append(item)
+			print(item)
+
+		next_page = soup.find("a", string="Próximo")
+
+		print('\n\n next',next_page)
+
+		has_next_page = next_page != None
+		if not has_next_page:
+			break
+		url =  next_page['href']
+		print('url next page', url)
+
+
+file_name = './dataset.json'
+# file = open(file_name,'w+',  encoding='utf8')
+
+with open(file_name, 'w+') as file:
+	json.dump(items, file, ensure_ascii=False)
